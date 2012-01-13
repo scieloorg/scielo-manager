97611e884b7ee46bb7aa57cecc6d88e00c6e59b6
Gustavo Fonseca <gustavofons@gmail.com>
2012-01-13 14:09:29 -0200

    Remodelada a relação entre as classes persistentes Collection e UserProfile

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -9,10 +9,7 @@ import choices
 #import scielomanager.tools
 
 class Collection(models.Model):
-    class Meta:
-        ordering = ['name']
     name = models.CharField(_('Collection Name'), max_length=128, db_index=True,)
-    manager = models.ForeignKey(User, related_name='collection_user')
     url = models.URLField(_('Instance URL'), )
     validated = models.BooleanField(_('Validated'), default=False, )
 
@@ -25,11 +22,9 @@ class Collection(models.Model):
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     collection = models.ForeignKey(Collection, related_name='user_collection', blank=False)
+    is_manager = models.BooleanField(_('Is manager of the collection?'), default=False, null=False, blank=True)
 
 class Institution(models.Model):
-    class Meta:
-        ordering = ['name']
-
     name = models.CharField(_('Institution Name'), max_length=128, db_index=True)
     acronym = models.CharField(_('Sigla'), max_length=16, db_index=True, blank=True)
     collection = models.ForeignKey(Collection, related_name='publisher_collection')
@@ -49,18 +44,10 @@ class Institution(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
+    class Meta:
+        ordering = ['name']
 
 class Journal(models.Model):
-    def __unicode__(self):
-        return u'%s' % (self.title)
-
-    class Meta:
-        ordering = ['title']
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ("/title")
-
     # PART 1
     creator = models.ForeignKey(User, related_name='enjoy_creator', editable=False)
     created = models.DateTimeField(_('Date of Registration'),default=datetime.now,
@@ -110,6 +97,16 @@ class Journal(models.Model):
         choices=choices.PUBLICATION_LEVEL,null=False,blank=True)
     secs_code = models.CharField(_('SECS Code'), max_length=64,null=False,blank=True)
     validated = models.BooleanField(_('Validated'), default=False,null=False,blank=True )
+
+    def __unicode__(self):
+        return u'%s' % (self.title)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("/title")
+
+    class Meta:
+        ordering = ['title']
 
 class JournalMission(models.Model):
     journal = models.ForeignKey(Journal,null=False)
