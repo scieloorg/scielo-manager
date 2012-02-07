dd88eb105049b83a75e8b7b09b95df98bf7678b8
Jamil Atta Junior <atta.jamil@gmail.com>
2012-02-07 18:31:01 -0200

    Ticket #140 e #111

--- scielomanager/journalmanager/views.py
+++ scielomanager/journalmanager/views.py
@@ -247,7 +247,7 @@ def show_institution(request, institution_id):
 @login_required
 def institution_index(request):
     user_collection = request.user.userprofile_set.get().collection
-    all_institutions = models.Institution.objects.available(True).filter(collection = user_collection)
+    all_institutions = models.Institution.objects.available(request.GET.get('is_available', 1)).filter(collection = user_collection)
 
     institutions = get_paginated(all_institutions, request.GET.get('page', 1))
 
@@ -318,7 +318,7 @@ def issue_index(request, journal_id):
     journal = models.Journal.objects.get(pk = journal_id)
     user_collection = request.user.userprofile_set.get().collection
 
-    all_issues = models.Issue.objects.available(True).filter(journal = journal_id)
+    all_issues = models.Issue.objects.available(request.GET.get('is_available', 1)).filter(journal = journal_id)
 
     issues = get_paginated(all_issues, request.GET.get('page', 1))
 
@@ -382,7 +382,7 @@ def search_journal(request):
     user_collection = request.user.userprofile_set.get().collection
 
     #Get journals where title contains the "q" value and collection equal with the user
-    journals_filter = models.Journal.objects.available(True).filter(title__icontains = request.REQUEST['q'],
+    journals_filter = models.Journal.objects.filter(title__icontains = request.REQUEST['q'],
                                                     collections = user_collection).order_by('title')
 
     #Paginated the result
@@ -401,7 +401,7 @@ def search_institution(request):
     user_collection = request.user.userprofile_set.get().collection
 
     #Get institutions where title contains the "q" value and collection equal with the user
-    institutions_filter = models.Institution.objects.available(True).filter(name__icontains = request.REQUEST['q'],
+    institutions_filter = models.Institution.objects.filter(name__icontains = request.REQUEST['q'],
                                                             collection = user_collection).order_by('name')
 
     #Paginated the result
@@ -421,7 +421,7 @@ def search_issue(request, journal_id):
     journal = models.Journal.objects.get(pk = journal_id)
     user_collection = request.user.userprofile_set.get().collection
     #Get issues where journal.id = journal_id and volume contains "q"
-    selected_issues = models.Issue.objects.available(True).filter(journal = journal_id,
+    selected_issues = models.Issue.objects.filter(journal = journal_id,
                                                   volume__icontains = request.REQUEST['q']).order_by('publication_date')
 
     #Paginated the result
