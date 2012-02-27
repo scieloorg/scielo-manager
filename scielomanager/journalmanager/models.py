c165fd310e1afa29db371eee40ddcf798f5bdf11
Ednilson Gesseff <ednilson.gesseff@scielo.org>
2012-02-27 14:34:04 -0300

    #138 - Campo Subscription removido de Journal

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -137,7 +137,6 @@ class Journal(models.Model):
     url_main_collection = models.CharField(_('URL of main collection'), max_length=64,null=True,blank=True)
     url_online_submission = models.CharField(_('URL of online submission'), max_length=64,null=True,blank=True)
     url_journal = models.CharField(_('URL of the journal'), max_length=64,null=True,blank=True)
-    subscription = models.CharField(_('Subscription'), max_length=4, null=False, default='na', blank=True, choices=choices.SUBSCRIPT)
 
     notes = models.TextField(_('Notes'), max_length=254, null=True, blank=True)
 
