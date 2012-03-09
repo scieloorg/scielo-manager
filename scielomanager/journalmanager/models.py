3dd5eaef03614b828482751ad652ec92d33b99a4
Fabio Batalha <fabiobcs@yahoo.com.br>
2012-03-09 17:19:21 -0300

    Ajustes finais

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -260,8 +260,8 @@ class Supplement(Issue):
 class Center(Institution):
     objects = AppCustomManager()
 
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+#def create_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        UserProfile.objects.create(user=instance)
 
-post_save.connect(create_user_profile, sender=User)
+#post_save.connect(create_user_profile, sender=User)
