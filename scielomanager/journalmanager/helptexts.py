9c7baf76f62da79151560b97914768854bb36601
Ednilson Gesseff <ednilson.gesseff@scielo.org>
2012-03-09 18:39:49 -0300

    Adiçao de icones de ajuda - Journal concluido - #163

--- scielomanager/journalmanager/helptexts.py
+++ scielomanager/journalmanager/helptexts.py
@@ -7,19 +7,70 @@ JOURNAL__ACRONYM = _("""Enter in this field the acronym of the journal, max 8 ch
 
 JOURNAL__ALPHABET = _("""Enter data in this field the alphabet of the Original Title of the journal title.""")
 
+JOURNAL__CENTER = _("""Code which identifies the center responsible for the creation of the bibliographic records.""")
+
 JOURNAL__COPYRIGHTER = _("""Enter a Copyrighter.""")
 
 JOURNAL__CTRL_VOCABULARY = _("""Enter in this field the controlled vocabulary used to the key words.""")
 
 JOURNAL__EDITORIAL_STANDARD = _("""Standard used to the bibliographic references presentation.""")
 
+JOURNAL__ELETRONIC_ISSN = _("""Eletronic ISSN.""")
+
 JOURNAL__FINAL_NUM = _("""Enter the final number in arabic numerals.""")
 
 JOURNAL__FINAL_VOL = _("""Number of the last published volume of the serial.""")
 
+JOURNAL__FINAL_YEAR = _("""Enter the date of the last issue of the journal in ISO format.""")
+
 JOURNAL__FREQUENCY = _("""Code which identifies the intervals of time in which the issues of the serial are published.""")
 
+JOURNAL__INIT_NUM = _("""Enter the initial number in arabic numerals.""")  
+
+JOURNAL__INIT_YEAR = _("""Enter the date of the first issue of the journal in ISO format.""")
+
+JOURNAL__INIT_VOL = _("""Enter the initial volume in arabic numerals.""")
+
+JOURNAL__LITERATURE_TYPE = _("""Type of literature""")
+
+JOURNAL__NATIONAL_CODE = _("""Code which identifies the title in the national system of serials of each country""")
+
+JOURNAL__NOTES = _("""Enter in this field, in free style, all information of interest to the Center about the serial.""")
+
+JOURNAL__PREVIOUS_TITLE = _("""Previous title in the language and form in which it appears.""")
+
+JOURNAL__PRINT_ISSN = _("""Print ISSN.""")
+
+JOURNAL__PUB_LEVEL = _(""" Publication Level """)
+
+JOURNAL__PUB_STATUS = _("""Date and status which identify the changes of the journal status in the collection.""")
+
+JOURNAL__PUBLISHER = _("""Name of the publisher and/or issuing body of the serial.""")
+
+JOURNAL__SCIELO_ISSN = _("""Enter the current ISSN. It can be different or equal to the ISSN used as ID, but it 
+     is usually the online ISSN.""")
+
+JOURNAL__SPONSOR = _("""Sponsor... """)
+
+JOURNAL__SUBJECT_DESCRIPTORS = _("""Standard terms used for the subject representation of the serial being described.""")
+
 JOURNAL__TITLE = _("""Main title or title proper of the serial, in the language and form in which it appears.""")
+
+JOURNAL__TREATMENT_LEVEL = _("""Treatment Type""")
+
+JOURNAL__URL_MAIN_COLLECTION = _("""Enter the URL of the main collection site. If the journal is published in SciELO Brazil, then www.scielo.br """)
+
+JOURNAL__URL_ONLINE_SUBMISSION= _("""Enter the URL of the submission site.""")
+
+JOURNAL__URL_JOURNAL = _("""If the journal is published in another site, enter in this field the other site location.""")
+
+JOURNAL__USE_LICENSE = _("""Use license...""")
+
+JOURNALCOLLECTIONS_COLLECTION = _("""Select the collections to which the journal belong.""")
+
+JOURNALINDEXCOVERAGE__DATABASE = _("""Select the database in which the title is indexed.""")
+
+JOURNALMISSION_DESCRIPTION = _("""Enter in this field the mission of the journal in English, Spanish and Portuguese.""")
 
 JOURNALSTUDYAREA__STUDYAREA = _("""Select one or more subjects related to the journal.""")
 
