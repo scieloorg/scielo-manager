8a6a6a5290ea0170a8f22e645a1ee3a0808a23e7
Gustavo Fonseca <gustavofons@gmail.com>
2012-03-20 14:36:56 -0300

    Ajustes para build no travis

--- scielomanager/settings.py
+++ scielomanager/settings.py
@@ -15,18 +15,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# Database configuration must be done at settings_local.include
-DATABASES = {
-    'default': {
-        'ENGINE': 'postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'journalmanager',         # Or path to database file if using sqlite3.
-        'USER': 'postgres',               # Not used with sqlite3.
-        'PASSWORD': '',                   # Not used with sqlite3.
-        'HOST': '',                       # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
