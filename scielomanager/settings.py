afb2b698139ae60f4f91717403944cb4bf01c35f
Gustavo Fonseca <gustavofons@gmail.com>
2012-03-20 11:29:33 -0300

    Voltei o LANGUAGE_CODE para pt-br por hora. No momento que trocar para en devemos ajustar os testes automatizados.

--- scielomanager/settings.py
+++ scielomanager/settings.py
@@ -26,7 +26,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
