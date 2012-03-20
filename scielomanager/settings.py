e9ef5aefed49c0df91e3f5dd0d98d7826ce5a58c
Gustavo Fonseca <gustavofons@gmail.com>
2012-03-20 10:57:35 -0300

    Movido o context_processor para o pacote de extensões do scielo

--- scielomanager/settings.py
+++ scielomanager/settings.py
@@ -137,7 +137,7 @@ TEMPLATE_CONTEXT_PROCESSORS =(
     'django.core.context_processors.media',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
-    'journalmanager.context_processors.from_settings',
+    'scielo_extensions.context_processors.from_settings',
 )
 
 # Messages framework
