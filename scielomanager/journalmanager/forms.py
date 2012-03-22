af13459f654fb5dfaa5a7306dfda1ee7c2461458
Gustavo Fonseca <gustavofons@gmail.com>
2012-03-22 18:04:02 -0300

    Alterado o campo Journal.collections, antes um inlineformset e agora um relacionamento manytomany convencional

--- scielomanager/journalmanager/forms.py
+++ scielomanager/journalmanager/forms.py
@@ -173,31 +173,6 @@ class UserCollectionsForm(ModelForm):
         'collection':forms.Select(attrs={'class':'span10', 'rows':'3'}),
       }
 
-# class JournalCollectionsForm(ModelForm):
-#     collection = forms.ModelChoiceField(models.JournalCollections.objects.none(), required=True)
-
-#     def __init__(self, *args, **kwargs):
-#         """
-#         Collection field queryset is overridden to display only
-#         collections related to a user.
-
-#         ``collections_qset`` should not be passed to the superclass
-#         ``__init__`` method.
-#         """
-#         collections_qset = kwargs.pop('collections_qset', None)
-#         super(JournalCollectionsForm, self).__init__(*args, **kwargs)
-
-#         if collections_qset is not None:
-#             self.fields['collection'].queryset = models.Collection.objects.filter(pk__in = (collection.collection.pk for collection in collections_qset))
-
-
-#     class Meta:
-#       model = models.JournalCollections
-#       widgets = {
-#         'collection':forms.Select(attrs={'class':'span10', 'rows':'3'}),
-#       }
-
-
 class JournalStudyAreaForm(ModelForm):
     class Meta:
       model = models.JournalStudyArea
