7558395bdd0b95590994cf3bf9ab9b8912f69193
Jamil Atta Junior <atta.jamil@gmail.com>
2012-05-07 14:14:19 -0300

    tk195

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -141,7 +141,7 @@ class Journal(caching.base.CachingMixin, models.Model):
     publisher = models.ForeignKey('Publisher', related_name='journal_institution',null=False, help_text=helptexts.JOURNAL__PUBLISHER)
     previous_title = models.ForeignKey('Journal',related_name='prev_title', null=True, blank=True, help_text=helptexts.JOURNAL__PREVIOUS_TITLE)
     use_license = models.ForeignKey('UseLicense', null=True, blank=False, help_text=helptexts.JOURNAL__USE_LICENSE)
-    collections = models.ManyToManyField('Collection', help_text=helptexts.JOURNAL__COLLECTIONS) #ajustar ref do help_text
+    collections = models.ManyToManyField('Collection', help_text=helptexts.JOURNAL__COLLECTIONS)
     languages = models.ManyToManyField('Language')
 
     #Fields
@@ -151,7 +151,7 @@ class Journal(caching.base.CachingMixin, models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     acronym = models.CharField(_('Acronym'),max_length=8, blank=False, help_text=helptexts.JOURNAL__ACRONYM)
-    scielo_issn = models.CharField(_('Which ISSN is in use at SciELO?'),max_length=16,
+    scielo_issn = models.CharField(_('The ISSN used to build the Journal PID.'),max_length=16,
         choices=choices.SCIELO_ISSN,null=False,blank=True, help_text=helptexts.JOURNAL__SCIELO_ISSN)
     print_issn = models.CharField(_('Print ISSN'),max_length=9,null=False,blank=True, help_text=helptexts.JOURNAL__PRINT_ISSN)
     eletronic_issn = models.CharField(_('Eletronic ISSN'),max_length=9,null=False,blank=True, help_text=helptexts.JOURNAL__ELETRONIC_ISSN)
