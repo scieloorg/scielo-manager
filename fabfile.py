1458b7543d3d7a2471f936beefa91428f92dbba2
Gustavo Fonseca <gustavofons@gmail.com>
2012-08-21 14:43:53 -0300

    Script fabric ajustado para reiniciar o apache

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,33 @@ from fabric.api import (
     run,
     cd,
     prefix,
+    sudo,
 )
+from fabric.context_managers import settings
 
-import fabfile_settings as settings
+import fabfile_settings as app_settings
+
+
+@task
+def restart_ws():
+    """
+    Restart the remote Web Server.
+    """
+    cmd = '%s' % app_settings.WEBSERVICE_RESTART_CMD
+    with settings(warn_only=True):
+        response_buff = run(cmd, combine_stderr=False)
+        if response_buff.stderr:
+            sudo(cmd)
 
 
 @task
 def update():
-    with cd(settings.INSTALLATION_PATH):
-        with prefix('source %s/bin/activate' % settings.VENV_PATH):
+    """
+    Update an instance running on a remote server.
+    """
+    with cd(app_settings.INSTALLATION_PATH):
+        with prefix('source %s/bin/activate' % app_settings.VENV_PATH):
             run('git pull origin master')
             run('make upgrade')
+
+    restart_ws()
