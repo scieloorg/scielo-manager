0eccd19b4c8fdfeb11583dbc581587b5e756cdd3
Gustavo Fonseca <gustavofons@gmail.com>
2012-10-04 15:33:01 -0300

    Ajustes no HTML. Removidas algumas estruturas inúteis

--- scielomanager/journalmanager/tests/tests_screen_components.py
+++ scielomanager/journalmanager/tests/tests_screen_components.py
@@ -12,7 +12,7 @@ from journalmanager.tests import modelfactories
 HASH_FOR_123 = 'sha1$93d45$5f366b56ce0444bfea0f5634c7ce8248508c9799'
 
 
-class CollectionsSelectorTests(WebTest):
+class UserCollectionsSelectorTests(WebTest):
 
     def test_auto_define_a_collection_as_default_when_it_is_the_unique(self):
         user = auth.UserF(is_active=True)
