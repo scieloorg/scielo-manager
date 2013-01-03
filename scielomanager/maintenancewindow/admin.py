c2ec70c0e202d1b7ba6b9df5b6f2df3be058c559
Gustavo Fonseca <gustavofons@gmail.com>
2013-01-03 16:32:05 -0200

    Mini-merge com o branch ``articles``

--- scielomanager/maintenancewindow/admin.py
+++ scielomanager/maintenancewindow/admin.py
@@ -7,7 +7,6 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'begin_at', 'end_at', 'is_blocking_users', 'is_finished')
 
     def save_model(self, request, obj, form, change):
-        #import pdb; pdb.set_trace()
         if obj.is_blocking_users == True:
             Event.objects.set_blocking_users_events_to_false()
         obj.save()
