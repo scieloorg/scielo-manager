5433be3eaddd299ad6ac95e163b95fc7e5ddfb91
Jamil Atta Junior <atta.jamil@gmail.com>
2013-01-11 14:47:29 -0200

    Infra-Estrutura para download dos arquivos *.mds

--- scielomanager/export/markupfiles.py
+++ scielomanager/export/markupfiles.py
@@ -2,11 +2,26 @@
 from django.conf import settings
 
 from scielomanager.export import bundle
-from scielomanager.journalmanager import choices
-
 
 MEDIA_ROOT = settings.MEDIA_ROOT + '/export/'
 MEDIA_URL = settings.MEDIA_URL + '/export/'
+
+standards = {
+    'iso690': ('icitat', 'iso', u'iso 690/87 - international standard organization'),
+    'nbr6023': ('acitat', 'abnt', u'nbr 6023/89 - associação nacional de normas técnicas'),
+    'other': ('ocitat', 'other', u'other standard'),
+    'vancouv': ('vcitat', 'vanc', u'the vancouver group - uniform requirements for manuscripts submitted to biomedical journals'),
+    'apa': ('pcitat', 'apa', u'American Psychological Association'),
+}
+
+L10ISSUEMGS = {'en': (u'No section title', u'No Descriptor', u'Health Sciences Descriptors'),
+        'es': (u'Sín título de sección', u'Ningun Descriptor', u'Descriptores en Ciencia de la Salud'),
+        'pt': (u'Sem título de seção', u'Nenhum Descritor', u'Descritores em Ciência da Saúde')}
+
+issns = {
+    'print': 'print_issn',
+    'electronic': 'eletronic_issn',
+}
 
 
 class GenerationError(Exception):
@@ -19,26 +34,13 @@ class Automata(object):
     Represents the automata.mds file
     http://ref.scielo.org/2qx6fb
     """
-    # {dbvalue: (citat, norma)}
-    standards = {
-        'iso690': ('icitat', 'iso', u'iso 690/87 - international standard organization'),
-        'nbr6023': ('acitat', 'abnt', u'nbr 6023/89 - associação nacional de normas técnicas'),
-        'other': ('ocitat', 'other', u'other standard'),
-        'vancouv': ('vcitat', 'vanc', u'the vancouver group - uniform requirements for manuscripts submitted to biomedical journals'),
-        'apa': ('pcitat', 'apa', u'American Psychological Association'),
-    }
-    # {dbvalue: journalmethod}
-    issns = {
-        'print': 'print_issn',
-        'electronic': 'eletronic_issn',
-    }
 
     def __init__(self, journal):
         self._journal = journal
 
     @property
     def citat(self):
-        tags = self.standards.get(self._journal.editorial_standard, None)
+        tags = standards.get(self._journal.editorial_standard, None)
         if not tags:
             return ''
 
@@ -53,7 +55,7 @@ class Automata(object):
 
     @property
     def norma_acron(self):
-        tags = self.standards.get(self._journal.editorial_standard, None)
+        tags = standards.get(self._journal.editorial_standard, None)
         if not tags:
             return ''
 
@@ -61,7 +63,7 @@ class Automata(object):
 
     @property
     def norma_name(self):
-        tags = self.standards.get(self._journal.editorial_standard, None)
+        tags = standards.get(self._journal.editorial_standard, None)
         if not tags:
             return ''
 
@@ -70,7 +72,7 @@ class Automata(object):
     @property
     def issn(self):
         pid_issn_field = self._journal.scielo_issn
-        pid_issn = getattr(self._journal, self.issns[pid_issn_field], None)
+        pid_issn = getattr(self._journal, issns[pid_issn_field], None)
         if not pid_issn:
             return ''
 
@@ -121,10 +123,6 @@ class Issue(object):
 
 
 class L10nIssue(Automata, Issue):
-
-    L10ISSUEMGS = {'en': (u'No section title', u'No Descriptor', u'Health Sciences Descriptors'),
-            'es': (u'Sín título de sección', u'Ningun Descriptor', u'Descriptores en Ciencia de la Salud'),
-            'pt': (u'Sem título de seção', u'Nenhum Descritor', u'Descritores em Ciência da Saúde')}
 
     def __init__(self, journal, issue, language):
         self._journal = journal
@@ -189,7 +187,7 @@ class L10nIssue(Automata, Issue):
     @property
     def sections(self):
         sections = ';'.join([unicode(section) for section in self._issue.section.all()])
-        return sections + u';' + self.L10ISSUEMGS[self._language][0] if sections else self.L10ISSUEMGS[self._language][0]
+        return sections + u';' + L10ISSUEMGS[self._language][0] if sections else L10ISSUEMGS[self._language][0]
 
     @property
     def sections_ids(self):
@@ -202,9 +200,9 @@ class L10nIssue(Automata, Issue):
         O vocabulário controlado deve ser traduzido
         '''
         if self._issue.journal.ctrl_vocabulary == 'decs':
-            return self.L10ISSUEMGS[self._language][2]
+            return L10ISSUEMGS[self._language][2]
         else:
-            return self.L10ISSUEMGS[self._language][1]
+            return L10ISSUEMGS[self._language][1]
 
     def __unicode__(self):
         rows = '\r\n'.join([
@@ -295,6 +293,231 @@ def generate(journal, issue):
         packmeta = [
             ('automata.mds', unicode(export_automata)),
             ('issue.mds', unicode(export_issue)),
+            ('en_issue.mds', unicode(export_l10n_issue_en)),
+            ('es_issue.mds', unicode(export_l10n_issue_es)),
+            ('pt_issue.mds', unicode(export_l10n_issue_pt)),
+            ('journal-standard.txt', unicode(export_journal_standard)),
+        ]
+    except AttributeError as exc:
+        raise GenerationError('it was impossible to generate the package for %s. %s' % (journal.pk, exc))
+    else:
+        pkg = bundle.Bundle(*packmeta)
+
+    pkg_filename = bundle.generate_filename('markupfiles', filetype='zip')
+
+    pkg.deploy(MEDIA_ROOT + pkg_filename)
+    return MEDIA_URL + pkg_filename
+
+
+class Ahead(object):
+
+    def __init__(self, journal, year):
+        self._journal = journal
+        self._year = year
+
+    @property
+    def legend(self):
+        return u'{0} n.ahead'.format(self._journal.title_iso)
+
+    @property
+    def period(self):
+        '''
+        O período deve ser o especificado pelo PMC.
+        EX.: Apr/Jun ou Apr/ ou /Jun
+        '''
+        return '%s/%s' % ('', '')
+
+    @property
+    def order(self):
+        '''
+        A propriedade order deve ser o publication_year concatenado com o order.
+        '''
+        return str(self._year) + '50'
+
+    def __unicode__(self):
+        rows = '\r\n'.join([self.legend, self.period, self.order, '', ''])
+        return rows
+
+
+class L10nAhead(Ahead):
+
+    def __init__(self, journal, year, language):
+        self._journal = journal
+        self._issue = None
+        self._year = year
+        self._language = language
+
+    @property
+    def short_title(self):
+        return self._journal.short_title
+
+    @property
+    def title_ahead(self):
+        return self.short_title + ' n.ahead ' + self._year
+
+    @property
+    def date_iso(self):
+        year = unicode(self._year)
+
+        if year:
+            return year + u'0000'
+        else:
+            return u''
+
+    @property
+    def status(self):
+        return u'1'
+
+    @property
+    def issn(self):
+        pid_issn_field = self._journal.scielo_issn
+        pid_issn = getattr(self._journal, issns[pid_issn_field], None)
+        if not pid_issn:
+            return ''
+
+        return pid_issn
+
+    @property
+    def sections(self):
+        return L10ISSUEMGS[self._language][0]
+
+    @property
+    def sections_ids(self):
+        return 'nd'
+
+    @property
+    def ctrl_vocabulary(self):
+        '''
+        O vocabulário controlado deve ser traduzido
+        '''
+        if self._journal.ctrl_vocabulary == 'decs':
+            return L10ISSUEMGS[self._language][2]
+        else:
+            return L10ISSUEMGS[self._language][1]
+
+    @property
+    def norma_name(self):
+        tags = standards.get(self._journal.editorial_standard, None)
+        if not tags:
+            return ''
+
+        return tags[2]
+
+    @property
+    def ahead_meta(self):
+        return u';'.join([
+            self.short_title,
+            '',
+            '',
+            'ahead',
+            '',
+            self.date_iso,
+            self.issn,
+            self.status,
+        ])
+
+    def __unicode__(self):
+        rows = '\r\n'.join([
+            self.title_ahead,
+            self.ahead_meta,
+            self.sections,
+            self.sections_ids,
+            self.ctrl_vocabulary,
+            self.norma_name,
+            '',
+        ])
+        return rows
+
+
+class JournalStandardAhead(L10nAhead):
+
+    def __init__(self, journal):
+        self._journal = journal
+
+    @property
+    def pub_type(self):
+        issns = {
+            'print': u'ppub',
+            'electronic': u'epub',
+        }
+        return issns[self._journal.scielo_issn]
+
+    @property
+    def study_area(self):
+        return '/'.join((area.study_area for area in self._journal.study_areas.all()))
+
+    @property
+    def medline_title(self):
+        return unicode(self._journal.title)
+
+    @property
+    def medline_code(self):
+        return u''
+
+    @property
+    def pissn(self):
+        return unicode(self._journal.print_issn)
+
+    @property
+    def eissn(self):
+        return unicode(self._journal.eletronic_issn)
+
+    @property
+    def publisher(self):
+        return unicode(self._journal.publisher_name)
+
+    @property
+    def title(self):
+        return unicode(self._journal.short_title)
+
+    @property
+    def acron(self):
+        return self._journal.acronym.lower()
+
+    @property
+    def norma(self):
+        if not self._journal.editorial_standard:
+            return ''
+        else:
+            return self._journal.editorial_standard
+
+    @property
+    def journal_meta(self):
+        return '#'.join([
+            self.issn,
+            self.title,
+            self.norma,
+            self.pub_type,
+            self.issn,
+            self.study_area,
+            self.medline_title,
+            self.medline_code,
+            self.title,
+            self.acron,
+            self.pissn,
+            self.eissn,
+            self.publisher,
+            ])
+
+    def __unicode__(self):
+        return self.journal_meta
+
+
+def generate_ahead(journal_id, year):
+    from scielomanager.journalmanager import models
+    journal = models.Journal.objects.get(id=journal_id)
+
+    export_automata = Automata(journal)
+    export_ahead = Ahead(journal, year)
+    export_l10n_issue_en = L10nAhead(journal, year, 'en')
+    export_l10n_issue_pt = L10nAhead(journal, year, 'pt')
+    export_l10n_issue_es = L10nAhead(journal, year, 'es')
+    export_journal_standard = JournalStandardAhead(journal)
+
+    try:
+        packmeta = [
+            ('automata.mds', unicode(export_automata)),
+            ('issue.mds', unicode(export_ahead)),
             ('en_issue.mds', unicode(export_l10n_issue_en)),
             ('es_issue.mds', unicode(export_l10n_issue_es)),
             ('pt_issue.mds', unicode(export_l10n_issue_pt)),
