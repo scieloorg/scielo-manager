db656540dcce6a5bf06023b6c3119b20a4f4fa29
Jamil Atta Junior <atta.jamil@gmail.com>
2013-01-30 10:38:01 -0200

    Considerando se é ou não press release na validação do cadastro de um issue

--- scielomanager/journalmanager/tests/tests_forms.py
+++ scielomanager/journalmanager/tests/tests_forms.py
@@ -1251,6 +1251,37 @@ class IssueFormTests(WebTest):
         self.assertIn('Saved.', response.body)
         self.assertTemplateUsed(response, 'journalmanager/issue_dashboard.html')
 
+    def test_press_release_of_existing_issue_can_be_created(self):
+        perm_issue_change = _makePermission(perm='add_issue',
+            model='issue', app_label='journalmanager')
+        perm_issue_list = _makePermission(perm='list_issue',
+            model='issue', app_label='journalmanager')
+        self.user.user_permissions.add(perm_issue_change)
+        self.user.user_permissions.add(perm_issue_list)
+
+        issue1 = modelfactories.IssueFactory(journal=self.journal,
+            volume='29', number='10', is_press_release=False)
+
+        form = self.app.get(reverse('issue.add',
+            args=[self.journal.pk]), user=self.user).forms[1]
+
+        form['total_documents'] = '16'
+        form.set('ctrl_vocabulary', 'decs')
+        form['number'] = str(issue1.number)
+        form['volume'] = str(issue1.volume)
+        form['editorial_standard'] = ''
+        form['is_press_release'] = True
+        form['publication_start_month'] = '9'
+        form['publication_end_month'] = '11'
+        form['publication_year'] = str(issue1.publication_year)
+        form['is_marked_up'] = False
+        form['editorial_standard'] = 'other'
+        form.set('use_license', str(issue1.journal.use_license.pk))
+
+        response = form.submit().follow()
+
+        self.assertIn('Saved.', response.body)
+        self.assertTemplateUsed(response, 'journalmanager/issue_dashboard.html')
 
     def test_form_enctype_must_be_multipart_formdata(self):
         """
