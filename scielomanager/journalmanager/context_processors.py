50b259c4a5dccdb95957648d4b24e4b534137af0
Gustavo Fonseca <gustavofons@gmail.com>
2013-02-21 21:16:27 -0300

    Otimização do total de queries realizadas pelos `context_processors`

--- scielomanager/journalmanager/context_processors.py
+++ scielomanager/journalmanager/context_processors.py
@@ -3,8 +3,6 @@ from django.conf import settings
 from journalmanager import models
 from maintenancewindow import models as maintenance_models
 
-from datetime import datetime
-
 
 def dynamic_template_inheritance(request):
     """
@@ -24,10 +22,10 @@ def show_system_notes(request):
     """
     Add system notes as maintenance events, notes, etc to the context
     """
+    def wrap():
+        return maintenance_models.Event.objects.scheduled_events()
 
-    system_notes = maintenance_models.Event.objects.scheduled_events()
-
-    return {'system_notes': system_notes}
+    return {'system_notes': wrap}
 
 
 def show_system_notes_blocking_users(request):
@@ -35,10 +33,10 @@ def show_system_notes_blocking_users(request):
     Add system notes that are blockin the user access
     as maintenance events, notes, etc to the context
     """
+    def wrap():
+        return maintenance_models.Event.objects.blocking_users_scheduled_event()
 
-    system_note = maintenance_models.Event.objects.blocking_users_scheduled_event()
-
-    return {'blocking_users_system_note': system_note}
+    return {'blocking_users_system_note': wrap}
 
 
 def on_maintenance(request):
@@ -46,8 +44,10 @@ def on_maintenance(request):
     Add on_maintenance item to the context. Defining if there is or not active
     maintenance events.
     """
+    def wrap():
+        return maintenance_models.Event.on_maintenance()
 
-    return {'on_maintenance': maintenance_models.Event.on_maintenance()}
+    return {'on_maintenance': wrap}
 
 
 def show_user_collections(request):
@@ -55,9 +55,11 @@ def show_user_collections(request):
     Adds `user_collections` item to the context, which is a
     queryset of collections the user relates to.
     """
+    def wrap():
+        return models.Collection.objects.all_by_user(request.user)
+
     if request.user.is_authenticated():
-        collections = models.Collection.objects.all_by_user(request.user)
-        return {'user_collections': collections}
+        return {'user_collections': wrap}
     else:
         return {}
 
@@ -66,11 +68,16 @@ def add_default_collection(request):
     if request.user.is_authenticated():
         try:
             collection = models.Collection.objects.get_default_by_user(request.user)
-            return {
-                'default_collection': collection,
-                'is_manager_of_default_collection': collection.is_managed_by_user(request.user)
-                }
         except models.Collection.DoesNotExist:
             return {}
+        else:
+
+            def wrap_is_managed_by_user():
+                return collection.is_managed_by_user(request.user)
+
+            return {
+                'default_collection': collection,
+                'is_manager_of_default_collection': wrap_is_managed_by_user
+                }
     else:
         return {}
