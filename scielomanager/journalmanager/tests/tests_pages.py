0802fdbc463a4ca4fb968c08ccc6a6e9c7daad13
Gustavo Fonseca <gustavofons@gmail.com>
2013-05-21 14:32:04 -0300

    Limpeza de códigos repetidos

--- scielomanager/journalmanager/tests/tests_pages.py
+++ scielomanager/journalmanager/tests/tests_pages.py
@@ -13,6 +13,10 @@ from journalmanager.tests.tests_forms import _makePermission
 
 
 def _makeUserRequestContext(user):
+    """
+    Constructs a class to be used by settings.USERREQUESTCONTEXT_FINDER
+    bound to a user, for testing purposes.
+    """
     class UserRequestContextTestFinder(object):
 
         def get_current_user_collections(self):
@@ -22,7 +26,37 @@ def _makeUserRequestContext(user):
             colls = self.get_current_user_collections()
             if colls:
                 return colls.get(usercollections__is_default=True)
+
     return UserRequestContextTestFinder
+
+
+def _patch_userrequestcontextfinder_settings_setup(func):
+    """
+    Patch the setting USERREQUESTCONTEXT_FINDER to target a
+    testing-friendly version.
+    """
+    def wrapper(self, **kwargs):
+        func(self, **kwargs)
+
+        # override the setting responsible for retrieving the active user context
+        cls = self.__class__
+        cls.UserRequestContextTestFinder = _makeUserRequestContext(self.user)
+        self.default_USERREQUESTCONTEXT_FINDER = settings.USERREQUESTCONTEXT_FINDER
+        settings.USERREQUESTCONTEXT_FINDER = 'scielomanager.scielomanager.journalmanager.tests.tests_pages.%s.UserRequestContextTestFinder' % cls.__name__
+
+    return wrapper
+
+
+def _patch_userrequestcontextfinder_settings_teardown(func):
+    """
+    Restore the settings defaults.
+    """
+    def wrapper(self, **kwargs):
+        func(self, **kwargs)
+
+        settings.USERREQUESTCONTEXT_FINDER = self.default_USERREQUESTCONTEXT_FINDER
+
+    return wrapper
 
 
 class UserCollectionsSelectorTests(WebTest):
@@ -156,19 +190,18 @@ class JournalsListTests(WebTest):
 
 class PressReleasesListTests(WebTest):
 
+    @_patch_userrequestcontextfinder_settings_setup
     def setUp(self):
         self.user = auth.UserF(is_active=True)
 
         self.collection = modelfactories.CollectionFactory.create()
         self.collection.add_user(self.user, is_manager=True, is_default=True)
 
-        # override the setting responsible for retrieving the active user context
-        PressReleasesListTests.UserRequestContextTestFinder = _makeUserRequestContext(self.user)
-        self.default_USERREQUESTCONTEXT_FINDER = settings.USERREQUESTCONTEXT_FINDER
-        settings.USERREQUESTCONTEXT_FINDER = 'scielomanager.scielomanager.journalmanager.tests.tests_pages.PressReleasesListTests.UserRequestContextTestFinder'
-
+    @_patch_userrequestcontextfinder_settings_teardown
     def tearDown(self):
-        settings.USERREQUESTCONTEXT_FINDER = self.default_USERREQUESTCONTEXT_FINDER
+        """
+        Restore the default values.
+        """
 
     def test_pressrelease_list_without_itens(self):
         """
