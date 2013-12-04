b4d4a15c5fba30d7fc85353c1780137f74492708
Jamil Atta Junior <atta.jamil@gmail.com>
2013-12-04 18:05:24 -0200

    Ajuste para que não tenhamos ID estáticos.

--- scielomanager/articletrack/tests/tests_pages.py
+++ scielomanager/articletrack/tests/tests_pages.py
@@ -22,11 +22,13 @@ def _makePermission(perm, model, app_label='articletrack'):
 class CheckinListTests(WebTest):
 
     def _makeOne(self):
-        self.checkin = modelfactories.CheckinFactory.create()
+        checkin = modelfactories.CheckinFactory.create()
 
         #Get only the first collection and set to the user
-        collection = self.checkin.journals.all()[0].collection
+        collection = checkin.journals.all()[0].collection
         collection.add_user(self.user, is_manager=True, is_default=True)
+
+        return checkin
 
     def _addWaffleFlag(self):
         Flag.objects.create(name='articletrack', authenticated=True)
@@ -65,55 +67,55 @@ class CheckinListTests(WebTest):
 
     def test_checkin_list_display_detail_and_history_button(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get('/arttrack/', user=self.user)
 
-        response.mustcontain('<a href="/arttrack/notice/1/" class="btn btn-mini btn" type="button">Details</a>')
-        response.mustcontain('<a href="/arttrack/package/1/" class="btn btn-mini btn" type="button">History</a>')
+        response.mustcontain('<a href="/arttrack/notice/%s/" class="btn btn-mini btn" type="button">Details</a>' % checkin.id)
+        response.mustcontain('<a href="/arttrack/package/%s/" class="btn btn-mini btn" type="button">History</a>' % checkin.id)
 
     def test_status_code_package_history(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get(reverse('checkin_history',
-            args=[self.checkin.pk]), user=self.user)
+            args=[checkin.pk]), user=self.user)
 
         self.assertEqual(response.status_code, 200)
 
     def test_package_history_index_with_itens(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get(reverse('checkin_history',
-            args=[self.checkin.pk]), user=self.user)
+            args=[checkin.pk]), user=self.user)
 
         response.mustcontain('20132404.zip')
 
     def test_package_history_index_have_article_title(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get(reverse('checkin_history',
-            args=[self.checkin.pk]), user=self.user)
+            args=[checkin.pk]), user=self.user)
 
         response.mustcontain('An azafluorenone alkaloid and a megastigmane from ...')
 
     def test_package_history_must_have_link_to_checkin_history(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get(reverse('checkin_history',
-            args=[self.checkin.pk]), user=self.user)
+            args=[checkin.pk]), user=self.user)
 
         response.mustcontain('<a href="/arttrack/">List of check ins</a>')
 
     def test_package_history_must_have_button_to_datail(self):
         self._addWaffleFlag()
-        self._makeOne()
+        checkin = self._makeOne()
 
         response = self.app.get(reverse('checkin_history',
-            args=[self.checkin.pk]), user=self.user)
+            args=[checkin.pk]), user=self.user)
 
         response.mustcontain('<a href="/arttrack/notice/1/" class="btn btn-mini btn" type="button">Details</a>')
 
@@ -121,11 +123,13 @@ class CheckinListTests(WebTest):
 class NoticeListTests(WebTest):
 
     def _makeOne(self):
-        self.notice = modelfactories.NoticeFactory.create()
+        notice = modelfactories.NoticeFactory.create()
 
         #Get only the first collection and set to the user
-        collection = self.notice.checkin.journals.all()[0].collection
+        collection = notice.checkin.journals.all()[0].collection
         collection.add_user(self.user, is_manager=True, is_default=True)
+
+        return notice
 
     def _addWaffleFlag(self):
         Flag.objects.create(name='articletrack', authenticated=True)
@@ -137,45 +141,45 @@ class NoticeListTests(WebTest):
 
     def test_status_code_notice_list(self):
         self._addWaffleFlag()
-        self._makeOne()
+        notice = self._makeOne()
 
         response = self.app.get(reverse('notice_detail',
-            args=[self.notice.checkin.pk]), user=self.user)
+            args=[notice.checkin.pk]), user=self.user)
 
         self.assertEqual(response.status_code, 200)
 
     def test_status_code_notice_list_without_waflle_flag(self):
-        self._makeOne()
+        notice = self._makeOne()
 
         response = self.app.get(reverse('notice_detail',
-            args=[self.notice.checkin.pk]), user=self.user, expect_errors=True)
+            args=[notice.checkin.pk]), user=self.user, expect_errors=True)
 
         self.assertEqual(response.status_code, 404)
 
     def test_notice_list_with_itens(self):
         self._addWaffleFlag()
-        self._makeOne()
+        notice = self._makeOne()
 
         response = self.app.get(reverse('notice_detail',
-            args=[self.notice.checkin.pk]), user=self.user)
+            args=[notice.checkin.pk]), user=self.user)
 
         response.mustcontain('The reference xyz is not ok')
 
     def test_notice_list_must_have_link_to_checkin_list(self):
         self._addWaffleFlag()
-        self._makeOne()
+        notice = self._makeOne()
 
         response = self.app.get(reverse('notice_detail',
-            args=[self.notice.checkin.pk]), user=self.user)
+            args=[notice.checkin.pk]), user=self.user)
 
         response.mustcontain('<a href="/arttrack/">List of check ins</a>')
 
     def test_notice_list_must_have_link_to_package_history(self):
         self._addWaffleFlag()
-        self._makeOne()
+        notice = self._makeOne()
 
         response = self.app.get(reverse('notice_detail',
-            args=[self.notice.checkin.pk]), user=self.user)
+            args=[notice.checkin.pk]), user=self.user)
 
-        response.mustcontain('<a href="/arttrack/package/1/">Package history</a>')
+        response.mustcontain('<a href="/arttrack/package/%s/">Package history</a>' % notice.id)
 
