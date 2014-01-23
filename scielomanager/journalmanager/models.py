a34a93f4b82dfdcd89bdccbb036e6a00e6ec6c14
Jamil Atta Junior <atta.jamil@gmail.com>
2014-01-23 14:14:21 -0200

    Criação do modelo Article no SciELO Manager

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -30,6 +30,7 @@ from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from scielo_extensions import modelfields
 from tastypie.models import create_api_key
+import jsonfield
 
 from scielomanager.utils import base28
 from . import modelmanagers
@@ -1095,6 +1096,28 @@ class AheadPressRelease(PressRelease):
     userobjects = modelmanagers.AheadPressReleaseManager()
 
     journal = models.ForeignKey(Journal, related_name='press_releases')
+
+
+class ArticleImage(caching.base.CachingMixin, models.Model):
+    objects = RegularPressReleaseCustomManager()
+    userobjects = modelmanagers.RegularPressReleaseManager()
+
+    image_url = models.CharField(_('Image URL'), max_length=256, db_index=True)
+
+
+class Article(caching.base.CachingMixin, models.Model):
+    """
+    Class that represent the article object
+    """
+
+    objects = caching.base.CachingManager()
+    nocacheobjects = models.Manager()
+
+    front = jsonfield.JSONField()
+    xml_url = models.CharField(_('XML URL'), max_length=256)
+    pdf_url = models.CharField(_('PDF URL'), max_length=256)
+    images_url = models.ManyToManyField(ArticleImage)
+
 
 ####
 # Pre and Post save to handle `Journal.pub_status` data modification.
