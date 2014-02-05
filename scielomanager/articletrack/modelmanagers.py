5f4871c1a8adcd7fe08682c38ccad7109be6063c
Juan Funez <juan.funez@MacBook-Pro-de-Administrador.local>
2014-02-05 16:31:38 -0200

    Sistema de tickets - Adiciono os modelos, managers, admin.py, e migrations
    O modelo Checkin foi divido em dois: Checkin e Article.

--- scielomanager/articletrack/modelmanagers.py
+++ scielomanager/articletrack/modelmanagers.py
@@ -1,9 +1,28 @@
 #coding: utf-8
 from journalmanager.modelmanagers import UserObjectQuerySet, UserObjectManager, user_request_context
 
+# CHECKIN
 
 class CheckinQuerySet(UserObjectQuerySet):
 
+    def all(self, get_all_collections=user_request_context.get_current_user_collections):
+        return self.filter(
+            article__journals__collection__in=get_all_collections()).distinct()
+
+    def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
+        return self.filter(
+            article__journals__collection=get_active_collection()).distinct()
+
+
+class CheckinManager(UserObjectManager):
+
+    def get_query_set(self):
+        return CheckinQuerySet(self.model, using=self._db)
+
+
+# ARTICLE
+
+class ArticleQuerySet(UserObjectQuerySet):
     def all(self, get_all_collections=user_request_context.get_current_user_collections):
         return self.filter(
             journals__collection__in=get_all_collections()).distinct()
@@ -13,7 +32,39 @@ class CheckinQuerySet(UserObjectQuerySet):
             journals__collection=get_active_collection()).distinct()
 
 
-class CheckinManager(UserObjectManager):
-
+class ArticleManager(UserObjectManager):
     def get_query_set(self):
-        return CheckinQuerySet(self.model, using=self._db)
+        return ArticleQuerySet(self.model, using=self._db)
+
+# TICKET
+
+class TicketQuerySet(UserObjectQuerySet):
+    def all(self, get_all_collections=user_request_context.get_current_user_collections):
+        return self.filter(
+            article__journals__collection__in=get_all_collections()).distinct()
+
+    def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
+        return self.filter(
+            article__journals__collection=get_active_collection()).distinct()
+
+
+class TicketManager(UserObjectManager):
+    def get_query_set(self):
+        return TicketQuerySet(self.model, using=self._db)
+
+
+# COMMENT
+
+class CommentQuerySet(UserObjectQuerySet):
+    def all(self, get_all_collections=user_request_context.get_current_user_collections):
+        return self.filter(
+            ticket__article__journals__collection__in=get_all_collections()).distinct()
+
+    def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
+        return self.filter(
+            ticket__article__journals__collection=get_active_collection()).distinct()
+
+
+class CommentManager(UserObjectManager):
+    def get_query_set(self):
+        return CommentQuerySet(self.model, using=self._db)
