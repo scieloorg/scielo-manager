f972cdf5b4f8849004ccdd602ea7523c84cbc4bf
Juan Funez <juan.funez@gmail.com>
2014-03-12 14:56:02 -0300

    Feedback do UI #654
    - Fix: label para status 'error', a passagem de parametros para este
    caso era considerada como False ou None, e não mostraba a label
    vermelha.

--- scielomanager/articletrack/templatetags/trans_status.py
+++ scielomanager/articletrack/templatetags/trans_status.py
@@ -1,12 +1,12 @@
 from django import template
-
+from scielomanager.tools import asbool
 register = template.Library()
 
 dict_status = {'ok': 'success'}
 label_status_translation = {'ok': 'success', 'error': 'important'}
 
 def trans_status(status, to_label=False):
-    if to_label:
+    if asbool(to_label):
         return label_status_translation.get(status,status)
     return dict_status.get(status, status)
 
