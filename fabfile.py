e7fb5fbfcf4fa6d7bdfcc5485c78fa32573031bd
Gustavo Fonseca <gustavo@gfonseca.net>
2014-03-24 19:02:32 -0300

    Ops, correção do fabfile.

--- fabfile.py
+++ fabfile.py
@@ -11,19 +11,19 @@ from fabric.context_managers import settings
 from fabfile_settings import deploy_envs as app_settings
 
 
-#@task
-#def restart_ws(env):
-#    """
-#    Restart the remote Web Server.
-#    """
-#    if env not in app_settings:
-#        exit('unknown deploy environment. is it configured in fabfile_settings.py?')
-#
-#    cmd = '%s' % app_settings[env]['webservice_restart_cmd']
-#    with settings(warn_only=True):
-#        response_buff = run(cmd, combine_stderr=False)
-#        if response_buff.stderr or 'permission denied' in response_buff.stdout.lower():
-#            sudo(cmd)
+@task
+def restart_ws(env):
+    """
+    Restart the remote Web Server.
+    """
+    if env not in app_settings:
+        exit('unknown deploy environment. is it configured in fabfile_settings.py?')
+
+    cmd = '%s' % app_settings[env]['webservice_restart_cmd']
+    with settings(warn_only=True):
+        response_buff = run(cmd, combine_stderr=False)
+        if response_buff.stderr or 'permission denied' in response_buff.stdout.lower():
+            sudo(cmd)
 
 
 @task
@@ -41,6 +41,5 @@ def update(env):
             run('git pull origin master')
             run('git stash pop')
             run('make upgrade')
-            run('circusctl restart')
 
-    #restart_ws(env)
+    restart_ws(env)
