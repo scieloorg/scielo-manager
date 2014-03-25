86113909858622eb1f59d9f317957df3fc89b541
fabiobatalha <fabiobatalha@gmail.com>
2014-03-25 15:14:25 -0300

    Corrigindo testes do articletrack para o novo modelo

--- scielomanager/articletrack/modelmanagers.py
+++ scielomanager/articletrack/modelmanagers.py
@@ -11,11 +11,11 @@ class CheckinQuerySet(UserObjectQuerySet):
 
     def all(self, get_all_collections=user_request_context.get_current_user_collections):
         return self.filter(
-            article__journals__collection__in=get_all_collections()).distinct()
+            article__journals__collections__in=get_all_collections()).distinct()
 
     def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
         return self.filter(
-            article__journals__collection=get_active_collection()).distinct()
+            article__journals__collections=get_active_collection()).distinct()
 
 
 class CheckinManager(UserObjectManager):
@@ -29,11 +29,11 @@ class CheckinManager(UserObjectManager):
 class ArticleQuerySet(UserObjectQuerySet):
     def all(self, get_all_collections=user_request_context.get_current_user_collections):
         return self.filter(
-            journals__collection__in=get_all_collections()).distinct()
+            journals__collections__in=get_all_collections()).distinct()
 
     def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
         return self.filter(
-            journals__collection=get_active_collection()).distinct()
+            journals__collections=get_active_collection()).distinct()
 
 
 class ArticleManager(UserObjectManager):
@@ -45,11 +45,11 @@ class ArticleManager(UserObjectManager):
 class TicketQuerySet(UserObjectQuerySet):
     def all(self, get_all_collections=user_request_context.get_current_user_collections):
         return self.filter(
-            article__journals__collection__in=get_all_collections()).distinct()
+            article__journals__collections__in=get_all_collections()).distinct()
 
     def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
         return self.filter(
-            article__journals__collection=get_active_collection()).distinct()
+            article__journals__collections=get_active_collection()).distinct()
 
 
 class TicketManager(UserObjectManager):
@@ -62,11 +62,11 @@ class TicketManager(UserObjectManager):
 class CommentQuerySet(UserObjectQuerySet):
     def all(self, get_all_collections=user_request_context.get_current_user_collections):
         return self.filter(
-            ticket__article__journals__collection__in=get_all_collections()).distinct()
+            ticket__article__journals__collections__in=get_all_collections()).distinct()
 
     def active(self, get_active_collection=user_request_context.get_current_user_active_collection):
         return self.filter(
-            ticket__article__journals__collection=get_active_collection()).distinct()
+            ticket__article__journals__collections=get_active_collection()).distinct()
 
 
 class CommentManager(UserObjectManager):
