d3b32d96d3570a5a895988b9a19340ce0f037728
Juan Funez <juan.funez@gmail.com>
2014-06-03 15:24:31 -0300

    Fix: edição de usuario, não validava email por estar repetido

--- scielomanager/journalmanager/forms.py
+++ scielomanager/journalmanager/forms.py
@@ -47,7 +47,7 @@ class UserChangeForm(UserChangeForm):
         Validates that the given email address is not used by another user.
         """
         email = self.cleaned_data["email"]
-        self.users_cache = User.objects.filter(email__iexact=email, is_active=True)
+        self.users_cache = User.objects.filter(email__iexact=email, is_active=True).exclude(pk=self.instance.pk)
         if len(self.users_cache) > 0:
             raise forms.ValidationError(USER_EMAIL_ERROR_MESSAGES)
         return email
