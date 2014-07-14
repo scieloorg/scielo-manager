9f83ec37fbfa15ace5924d76823b50ea7aa25a28
fabiobatalha <fabiobatalha@gmail.com>
2014-07-14 14:57:03 -0300

    Incluindo metodo para recuperar periódico que sucede de outro

--- scielomanager/journalmanager/models.py
+++ scielomanager/journalmanager/models.py
@@ -616,6 +616,13 @@ class Journal(caching.base.CachingMixin, models.Model):
 
         return grid
 
+    @property
+    def succeeding_title(self):
+        try:
+            return self.prev_title.get()
+        except ObjectDoesNotExist:
+            return None
+
     def has_issues(self, issues):
         """
         Returns ``True`` if all the given issues are bound to the journal.
