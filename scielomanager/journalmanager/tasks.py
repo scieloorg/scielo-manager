8fdb0bd8863b22480b9d6b1a49ed1c474de3525d
Juan Funez <juan.funez@gmail.com>
2015-02-02 17:15:14 -0200

    #1059 - criar task para associação entre: journalmanager.article e: journalmanager.issue
    - novo signal: ``link_issue_article`` no ``post_save`` do ``Article``
    - novas task: ``link_article_to_issue`` e ``process_orphan_articles``
    - ``process_orphan_articles``: recupera todos os Article sem Issue associado e repassa para ``link_article_to_issue``.
    - ``link_article_to_issue``: processa indivualmenete cada artigo e tenta procurar e associar o ``Article`` e o ``Issue``certo.

--- scielomanager/journalmanager/tasks.py
+++ scielomanager/journalmanager/tasks.py
@@ -53,3 +53,68 @@ def new_article_create_es_index(self, article_aid):
         result = es.index(index=ARTICLE_INDEX_NAME, doc_type=ARTICLE_DOC_TYPE, id=article_aid, body=article_data)
         logger.info('[new_article_create_es_index] RESULT: %s' % result)
     logger.debug('[new_article_create_es_index] FINISH task at %s' % datetime.datetime.now())
+
+
+@app.task(bind=True)
+def link_article_to_issue(self, article_aid):
+    """
+    Retrieve an article (by aid). then pick the article metadata to look for an issue to match.
+    """
+    logger.debug('[link_article_to_issue] START task (aid=%s) at %s' % (article_aid, datetime.datetime.now()))
+    # use of get_model to avoid circular import
+    Article = get_model('journalmanager', 'Article')
+    Journal = get_model('journalmanager', 'Journal')
+    Issue = get_model('journalmanager', 'Issue')
+
+    # retrieve article
+    try:
+        article = Article.objects.get(aid=article_aid)
+    except Article.DoesNotExist as e:
+        logger.error('[link_article_to_issue] FAIL while retrieving ARTICLE (aid: %s): %s' % (article_aid, e))
+    else:
+        if article.issue is None:
+            # retrieve journal
+            try:
+                journal = Journal.objects.get(title_iso=article.xml_abbrev_journal_title)
+            except Journal.DoesNotExist as e:
+                logger.error('[link_article_to_issue] FAIL while retrieving JOURNAL (aid: %s, xml_abbrev_journal_title: %s): %s' % (article_aid, article.xml_abbrev_journal_title, e))
+            except Journal.MultipleObjectsReturned as e:
+                logger.error('[link_article_to_issue] FAIL Multiple JOURNALS Returned for (aid: %s, xml_abbrev_journal_title: %s): %s' % (article_aid, article.xml_abbrev_journal_title, e))
+            else:
+
+                # retrieve issue
+                try:
+                    issue = Issue.objects.get(journal=journal, volume=article.xml_volume, number=article.xml_issue)
+                except Issue.DoesNotExist as e:
+                    logger.error('[link_article_to_issue] FAIL while retrieving ISSUE (aid: %s, xml_volume: %s, xml_issue: %s): %s' % (article_aid, article.xml_volume, article.xml_issue, e))
+                except Issue.MultipleObjectsReturned as e:
+                    logger.error('[link_article_to_issue] FAIL Multiple ISSUES Returned for (aid: %s, xml_volume: %s, xml_issue: %s): %s' % (article_aid, article.xml_volume, article.xml_issue, e))
+                else:
+                    # link issue and article:
+                    article.issue = issue
+                    article.save()
+                    logger.info('[link_article_to_issue] article has an issue now!: (aid: %s) -> (issue: %s)' % (article.aid, article.issue))
+        else:
+            logger.info('[link_article_to_issue] article has an issue already!: (aid: %s) -> (issue: %s)' % (article.aid, article.issue))
+
+    logger.debug('[link_article_to_issue] FINISH task at %s' % datetime.datetime.now())
+
+
+@app.task(bind=True)
+def process_orphan_articles(self):
+    """
+    collect every orphan articles (orphan == articles without an issue related), and for each call: link_article_to_issue.
+    This task is scheduled to run daily.
+    """
+    logger.debug("[process_orphan_articles] START process at %s" % datetime.datetime.now())
+    # use of get_model to avoid circular import
+    Article = get_model('journalmanager', 'Article')
+    articles = Article.objects.filter(issue=None)
+    logger.info('[process_orphan_articles] Found %s articles matching conditions' % articles.count())
+
+    for article in articles:
+        logger.info('[process_orphan_articles] Processing article (pk=%s, aid=%s)' % (article.pk, article.aid))
+        link_article_to_issue.apply_async(args=[article.aid, ])
+
+    logger.debug('[process_orphan_articles] FINISH process at %s' % datetime.datetime.now())
+
