2ac69c0d8c888b1e1032c3643fd354286d3fe032
Juan Funez <juan.funez@gmail.com>
2015-05-04 16:02:16 -0300

    #1083 - Remover a capacidade de validar XMLs remotos
    FIX: #1083

--- scielomanager/validator/forms.py
+++ scielomanager/validator/forms.py
@@ -5,21 +5,13 @@ from django.conf import settings
 from django.utils.translation import ugettext as _
 
 
-STYLECHECKER_TYPE_CHOICES = (
-    ('url', _('URL')),
-    ('file', _('File')),
-)
-
-
 class StyleCheckerForm(forms.Form):
-    type = forms.ChoiceField(label=_("Type"), choices=STYLECHECKER_TYPE_CHOICES, ) # widget=forms.RadioSelect
-    url = forms.URLField(label=_("URL"), required=False)
     file = forms.FileField(label=_("File"), required=False)
 
     def clean_file(self):
         _file = self.cleaned_data.get('file', None)
         if _file:
-            if _file.content_type not in ['text/xml', 'application/xml',]:
+            if _file.content_type not in ['text/xml', 'application/xml', ]:
                 raise forms.ValidationError(_(u"This type of file is not allowed! Please select another file."))
 
             if _file.size > settings.VALIDATOR_MAX_UPLOAD_SIZE:
@@ -28,12 +20,8 @@ class StyleCheckerForm(forms.Form):
         return _file
 
     def clean(self):
-        type = self.cleaned_data['type']
-        url = self.cleaned_data.get('url', None)
         file = self.cleaned_data.get('file', None)
 
-        if type == 'url' and not url:
-            raise forms.ValidationError('if trying to validate via URL, please submit a valid URL')
         if type == 'file' and not file:
             raise forms.ValidationError('if trying to validate a File, please upload a valid XML file')
 
