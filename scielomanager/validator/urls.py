e93d9170379601d8fda9a39ebceba65a01fac8b2
Juan Funez <juan.funez@gmail.com>
2015-07-29 18:20:08 -0300

    Interface para previsualizar HMTL subindo arquivos XML, e limpeza da app validator

--- scielomanager/validator/urls.py
+++ scielomanager/validator/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls.defaults import *
 from . import views
 
-urlpatterns = patterns('',
-	url(r'^stylechecker/$', views.packtools_home, name="validator.packtools.stylechecker"),
+urlpatterns = patterns(
+    '',
+    url(r'^stylechecker/$', views.packtools_home, name="validator.packtools.stylechecker"),
+    url(r'^preview/html/$', views.packtools_preview_html, name="validator.packtools.preview_html"),
 )
