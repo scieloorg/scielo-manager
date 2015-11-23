8e5b74caa41367cbe00e5397df59a04aa852891c
Gustavo Fonseca <gustavo@gfonseca.net>
2015-11-23 17:07:46 -0200

    Ajusta pontos da interação com o Elasticsearch
    Modificações para acomodar os atributos `links_to` e `referrers`.
    Ainda relacionado ao ticket #1222.

--- scielomanager/scielomanager/connectors/__init__.py
+++ scielomanager/scielomanager/connectors/__init__.py
@@ -4,15 +4,9 @@
 Um conector é apenas um link entre dois ou mais elementos conectáveis (e.g.,
 portas, interfaces etc).
 """
-import os
-
 # se apoia na definição da variável __all__ nos módulos.
 from .storage import *
 
 
-_CWD = os.path.dirname(os.path.abspath(__file__))
-ES_ARTICLE_MAPPING_PATH = os.path.join(_CWD, 'article_mapping.json')
-
-
-__all__ = (storage.__all__, ES_ARTICLE_MAPPING_PATH)
+__all__ = (storage.__all__, )
 
